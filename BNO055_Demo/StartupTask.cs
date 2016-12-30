631aff7367b8debd2b8efd007c1b0c7e08e07d88
Rick Lesniak <rick.lesniak@embedded-designs.com>
2016-12-30 17:10:27 -0500

    Add project files.

---
+++ BNO055_Demo/StartupTask.cs
@@ -0,0 +1,63 @@
+﻿/*------------------------------------------------------------------------
+  Adafruit BNO055 Demo for Windows Core IoT: BNO055 IMU chip.
+
+  Written by Rick Lesniak for Adafruit Industries.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  ------------------------------------------------------------------------
+  This file is intended to work with the Adafruit Class Library
+
+  This code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  MIT license, all text above must be included in any redistribution.
+  ------------------------------------------------------------------------*/using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using AdafruitClassLibrary;
+using System.Threading.Tasks;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace BNO055_Demo
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        BNO055 bno055 { get; set; }
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // If you start any asynchronous methods here, prevent the task
+            // from closing prematurely by using BackgroundTaskDeferral as
+            // described in http://aka.ms/backgroundtaskdeferral
+            //
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+            bno055 = new BNO055();
+
+            await bno055.InitBNO055Async(BNO055.OperationMode.OPERATION_MODE_NDOF, "UART0", 18);
+
+            BNO055.SystemStatus sysStatus = bno055.GetSystemStatus(true);
+            System.Diagnostics.Debug.WriteLine(string.Format("System Status: Status {0}, Self-Test {1}, Error {2}", sysStatus.StatusReg, sysStatus.SelfTestResult, sysStatus.ErrorReg));
+
+            BNO055.Revisions revision = bno055.GetRevision();
+            System.Diagnostics.Debug.WriteLine(string.Format("Revision: Software {0}, Bootloader {1}, Accel ID {2}, Mag ID {3}, Gyro ID {4}", revision.Software, revision.Bootloader, revision.AccelID, revision.MagID, revision.GyroID));
+            Task.Delay(1000).Wait();
+
+            while (true)
+            {
+                BNO055.Euler euler = bno055.ReadEuler();
+                if (null != euler)
+                    System.Diagnostics.Debug.WriteLine(string.Format("Euler: Pitch {0}, Roll {1}, Heading {2}", euler.Pitch, euler.Roll, euler.Heading));
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
